aaea86605d2d4c2ae1e653616b07878476de92b3
Moritz <>
2020-05-30 15:15:35 +0200

    Initialize application
    with Spring initializr

---
+++ src/main/java/cc/gameofthree/GameOfThreeApplication.java
@@ -0,0 +1,12 @@
+package cc.gameofthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOfThreeApplication.class, args);
+	}
+}
