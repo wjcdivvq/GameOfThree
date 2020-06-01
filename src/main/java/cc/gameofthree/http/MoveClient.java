8410caaed4b04731f0d7a612470e6b949a342f03
Moritz <>
2020-06-01 18:48:41 +0200

    Make url of second player configurable
    So that the app can play against another app, and not only with itself

--- src/main/java/cc/gameofthree/http/MoveClient.java
+++ src/main/java/cc/gameofthree/http/MoveClient.java
@@ -1,12 +1,17 @@
 package cc.gameofthree.http;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
 public class MoveClient {
-    private final WebClient webClient = WebClient.create("http://localhost:8080"); //TODO make configurable
+    private final WebClient webClient;
+
+    public MoveClient(@Value("${otherPlayerUrl:http://localhost:8080}") String otherPlayerUrl) {
+        this.webClient = WebClient.create(otherPlayerUrl);
+    }
 
     public Mono<Void> playerDidMove(int move) {
         return webClient.post()
